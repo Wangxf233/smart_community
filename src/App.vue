7556a1afcc8d3827c8b8b23b58936f6900630aee
Wangxf <494847203@qq.com>
2024-02-19 17:34:23 +0800

    '提交没有格式化的代码'

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>我是app根组件</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
